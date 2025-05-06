cc0db820c4f31701bfb435ae84d3e48f5cdd76a4
SUJATHA289 <SUJATHADEVI2891@GMAIL.COM>
2025-05-06 20:06:33 +0530

    Add files via upload

---
+++ models/git commands.sql
@@ -0,0 +1,52 @@
+git merge origin/qa
+git merge origin/STRY0511047
+git commit -am "resolve conflicts STRY0511047"
+git push origin qa_temp
+
+
+STRY0442493
+
+https://api55.sapsf.eu/odata/v2/Country?$format=json
+ 
+ 
+ 
+dbt build --select  dim_assignment_profile_referential+
+
+dbt build --select onboarding_process_tasks+ --full-refresh
+
+dbt build --select fact_positions_agg +
+
+
+hash(externalcode, effectivestartdate) as organizational_area_id,
+
+nvl(dbt_valid_to,'9999-12-31')
+
+8497937992
+Devireddy2891@1
+
+Snowflake2891@1
+
+
+infra/dbt/macros/onetime/stry0454746_assignment_profile_ref.sql
+
+dbt build --full-refresh -select fact_adoption_period_agg+
+
+RLSE0021643
+
+STRY0471671 - P1 - SDDS - Creation of the Pay Recurring object
+STRY0471672 - P1 -  SDDS - Creation of the Pay Non Recurring object
+
+
+STRY0480729 - P1 - Bug - Year End Salary Review | CompaRatio chart calculation After Campaign
+
+
+stg_emp_job.sql
+stg_emp_job_flatten.sql
+stg_position.sql
+stg_position_flatten.sql
+
+domain_restrictions.sql
+item_formula.sql
+schedule_resources.sql
+
+
